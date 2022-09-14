3e430be5df08eb1baa462ef2bd94dc1eff31dd1e
mnm458 <mohaknainmalhotra@gmail.com>
2022-09-14 14:50:47 +1000

    tests minor changes

--- tests/calculatordapp.js
+++ tests/calculatordapp.js
@@ -4,4 +4,12 @@ const {SystemProgram} = anchor.web3;
 
 describe('my calculator dapp', () => {
 
+  // Configure the client to use the local cluster.
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  // anchor.setProvider(anchor.Provider.env());
+  const calculator_keypair = anchor.web3.Keypair.generate();
+  const calculator_program = anchor.workspace.Calculatordapp;
+
+    
 });
