32f0db5ce9555f83448cc7b9ba9c45a8c6cf3f91
mnm458 <mohaknainmalhotra@gmail.com>
2022-09-14 14:55:50 +1000

    tests minor changes

--- tests/calculatordapp.js
+++ tests/calculatordapp.js
@@ -40,7 +40,7 @@ describe('my calculator dapp', () => {
     });
     console.log("Create new calculator account..!", tx);
     const account = await calculator_program.account.calculator.fetch(calculator_keypair.publicKey);
-  
+    assert.ok(account.result.eq(new anchor.BN(11)));
   });
 
 
