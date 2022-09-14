8f9fa8b695883875da33a2c01ca780c335a28c86
mnm458 <mohaknainmalhotra@gmail.com>
2022-09-14 11:28:09 +1000

    program struct

--- programs/src/lib.rs
+++ programs/src/lib.rs
@@ -2,3 +2,38 @@ use anchor_lang::prelude::*;
 // use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("HTsFVYZ81meXsJAgeqPZKsXLeCcC8A8GkRLGTAso9m4g");
+
+#[program]
+pub mod calculatordapp {
+    use super::*;
+    pub fn create(ctx: Context<Create>, init_message: String) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.message = init_message;
+        Ok(())
+    }
+
+    pub fn addition(ctx: Context<Addition>, num1: u32, num2: u32) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 as i64 + num2 as i64;
+        Ok(())
+    }
+
+    pub fn subtraction(ctx: Context<Subtraction>, num1: u32, num2: u32) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 as i64 - num2 as i64;
+        Ok(())
+    }
+
+    pub fn multiplication(ctx: Context<Multiplication>, num1: u32, num2: u32) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result= num1 as i64 * num2 as i64;
+        Ok(())
+    }
+
+    pub fn division(ctx: Context<Division>, num1: u32, num2: u32) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 as i64 / num2 as i64;
+        calculator.remander = num1 as i64 % num2 as i64;
+        Ok(())
+    }
+}
